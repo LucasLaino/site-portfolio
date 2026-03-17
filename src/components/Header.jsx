548c326b1f8b2acfaf55e91c5f296b56dcc2d4a2
Lucas Laino <lucaslaino00@gmail.com>
2026-03-17 20:34:02 -0300

    refactor: Adiciona fonte personalizada e altera o Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,39 @@
+import iconeGitHub from '../assets/img/github-white-icon.svg'
+import logo from '../assets/img/logo.png'
+
 export default function Header() {
     return (
-        <header className="flex justify-end p-5 h-[10dvh]">
-            <ul className="flex tracking-wide gap-5">
-                <li>
-                    <a href="#" className="p-2 rounded-full text-white font-medium bg-transparent hover:text-black hover:bg-yellow-300 transition duration-300 ease-in-out active:bg-yellow-400">Sobre mim</a>
+        <header className="flex justify-between items-center p-5 h-[10dvh] font-[Fonte_Rosto]">
+            <div>
+                <img 
+                    src={logo} 
+                    alt="logo L."
+                    width={32}
+                    height={32}
+                    className='rounded-lg'
+                />
+            </div>
+            
+            <ul className='flex items-center gap-3 tracking-wide'>
+                <li className='text-blue-300 p-2 rounded-full bg-blue-500/30 hover:bg-sky-700 transition ease-in-out'>
+                    <a href="#">Sobre</a>
                 </li>
-
-                <li>
-                    <a href="#" className="p-2 rounded-full text-white font-medium bg-transparent hover:text-black hover:bg-yellow-300 transition duration-300 ease-in-out active:bg-yellow-400">Projetos</a>
+                <li className='text-blue-300 p-2 rounded-full bg-blue-500/30 hover:bg-sky-700 transition ease-in-out'>
+                    <a href="#">Projetos</a>
                 </li>
-
+                <li className='text-blue-300 p-2 rounded-full bg-blue-500/30 hover:bg-sky-700 transition ease-in-out'>
+                    <a href="#">Contato</a>
+                </li>
                 <li>
-                    <a href="#" className="p-2 rounded-full text-white font-medium bg-transparent hover:text-black hover:bg-yellow-300 transition duration-300 ease-in-out active:bg-yellow-400">Contato</a>
+                    <a href="https://github.com/LucasLaino" target="_blank" rel="noopener noreferrer">
+                    
+                        <img src={iconeGitHub}
+                            alt="ícone Git Hub"
+                            width={42}
+                            height={42}
+                            className='p-2 rounded-lg bg-blue-500/30 hover:bg-sky-700 transition ease-in-out'
+                        />
+                    </a>
                 </li>
             </ul>
         </header>
