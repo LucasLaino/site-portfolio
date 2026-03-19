37f0a29c7817a721319eb86a5f180e8c7db19a9f
Lucas Laino <lucaslaino00@gmail.com>
2026-03-18 22:10:27 -0300

    refactor: Altera o Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,21 @@
-import fotoRosto from '../assets/fotoRosto.jpeg'
+import fotoRosto from '../assets/img/fotoRosto.jpeg'
 
 export default function Hero() {
   return (
-    <section className="relative h-[90dvh] flex items-center justify-between px-10">
-
-      <div className="flex flex-col justify-center max-w-1xl">
-        <p className="text-white font-medium text-7xl text-left">
-          Olá! Eu sou Lucas Laino.
-        </p>
-        <p className="text-white font-medium text-2xl text-left mt-4">
-          Desenvolvedor Web Fullstack - Entusiasta de IA & Automação
-        </p>
+    <section className="h-[90dvh] flex items-center justify-center font-[Fonte_Rosto]">
+      <div className='absolute z-50 w-[80%]'>
+        <p className="text-7xl text-white">Olá, eu sou <strong className='text-blue-300'>Lucas Laino</strong>.</p>
+        <p className='text-2xl text-white'>Desenvolvedor Web - Entusiasta de IA e Automação</p>
       </div>
 
-      <div className="h-full flex items-center justify-end py-15 pr-10">
+      <div className='fixed right-20 z-40'>
         <img
           src={fotoRosto}
-          alt="Foto do Lucas Laino"
-          className="max-h-full w-auto object-contain rounded-2xl"
+          alt="Retrato do rosto de Lucas Laino sorrindo"
+          width={450}
+          height={350}
         />
       </div>
-
     </section>
   );
 }
